cb87fd598325bd2d96a27a00645497555726399b
btiac1129 <btiac1129@gmail.com>
2021-08-21 18:34:19 +0900

    변수와 자료형

---
+++ Variable/src/variable.java
@@ -0,0 +1,54 @@
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 논리형 (boolean)
+		boolean bool1 = true;
+		boolean bool2 = false;
+		System.out.println("bool1 : " + bool1);
+		System.out.println("bool2 : " + bool2);
+	
+		// 문자형
+		char char1 = 65;
+		char char2 = 'A';
+		System.out.println("char1 : " + char1);
+		System.out.println("char2 : " + char2);
+	
+		// 정수형
+		byte byte1 = -128;
+		byte byte2 = 127;
+		System.out.println("byte1 : " + byte1);
+		System.out.println("byte2 : " + byte2);
+		
+		short short1 = -32768; // 2byte
+		short short2 = 32767;
+		System.out.println("short1 : " + short1);
+		System.out.println("short2 : " + short2);
+		
+		int int1 = -2147483648; // 4byte
+		int int2 = 2147483647;
+		System.out.println("int1 : " + int1);
+		System.out.println("int2 : " + int2);
+		
+		long long1 = -922337203684775808L; // 8byte
+		long long2 = 922337203684775807L;
+		System.out.println("long1 : " + long1);
+		System.out.println("long2 : " + long2);
+		
+		// 실수형
+		float float1 = -12.34f;
+		float float2 = 12.34f;
+		System.out.println("float1 : " + float1);
+		System.out.println("float2 : " + float2);
+		 
+		double double1 = 12.34;
+		double double2 = -12.34;
+		System.out.println("double1 : " + double1);
+		System.out.println("double2 : " + double2);
+		
+		// 문자열
+		
+	}
+
+}
